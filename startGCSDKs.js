d35518eb60be51aa2c73131e5d344b53c432150e
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-05-07 19:35:37 +0200

    Update startGCSDKs.js

--- startGCSDKs.js
+++ startGCSDKs.js
@@ -11,13 +11,28 @@ function startGCSDKs(clientId) {
         let userDetails = null;
         let gcHostOrigin = '';
         let conversationId = '';
-        assignConfiguration();
+        let gcHostURL = assignConfiguration();
 
-        const hostName = new URL(gcHostOrigin).hostname;
+        // Ensure the gcHostOrigin is a valid URL before creating a new URL instance
+        let hostName;
+        if (gcHostURL) {
+            try {
+                hostName = new URL(gcHostURL).hostname;
+            } catch (error) {
+                console.error("startGCSDKs.js - Invalid gcHostOrigin URL:", gcHostOrigin);
+                reject(error);
+                return;
+            }
+        } else {
+            console.log("startGCSDKs.js - gcHostOrigin is not set or invalid");
+            reject(new Error("gcHostOrigin is not set or invalid"));
+            return;
+        }
+
         const parts = hostName.split('.');
         parts.shift();
-        window.environment = parts.join('.'); //example enviroment value = "mypurecloud.ie" 
-        console.log('window.environment in startGCSDKs:', window.environment);
+        window.environment = parts.join('.'); // Example environment value = "mypurecloud.ie" 
+        console.log('startGCSDKs.js - window.environment in startGCSDKs:', window.environment);
 
         const platformClient = require('platformClient');
         const client = platformClient.ApiClient.instance;
@@ -53,55 +68,65 @@ function startGCSDKs(clientId) {
                 resolve(platformClient);
             })
             .catch((err) => {
-                console.error("Error during setup:", err);
+                console.error("startGCSDKs.js - Error during setup:", err);
                 reject(err);
             });
 
         function assignConfiguration() {
-            let browser_url = new URL(window.location);
-            let searchParams = new URLSearchParams(browser_url.search);
-            console.log('browser_url: ', browser_url);
-            console.log('searchParams: ', searchParams);
+            let browser_url = window.location.href;
+            if (!browser_url) {
+                console.log('startGCSDKs.js - Browser URL is null or undefined.');
+                return null;
+            }
+            let searchParams;
+            try {
+                let url = new URL(browser_url);
+                searchParams = new URLSearchParams(url.search);
+            } catch (error) {
+                console.error('startGCSDKs.js - Failed to parse the URL:', browser_url);
+                return null;
+            }
+            
+            console.log('startGCSDKs.js - browser_url: ', browser_url);
+            console.log('startGCSDKs.js - searchParams: ', searchParams);
             
             if (searchParams.has(qParamConversationId)) {
                 conversationId = searchParams.get(qParamConversationId);
                 window.conversationId = conversationId; 
-                console.log('Conversation ID set from URL parameter:', conversationId);
+                console.log('startGCSDKs.js - Conversation ID set from URL parameter:', conversationId);
             } else {
-                console.log('Conversation ID not found in URL parameters.');
+                console.log('startGCSDKs.js - Conversation ID not found in URL parameters.');
             }
 
             if (searchParams.has(qParamLanguage)) {
                 language = searchParams.get(qParamLanguage);
                 localStorage.setItem(`${appName}_language`, language);
-                console.log('Language set from URL parameter:', language);
+                console.log('startGCSDKs.js - Language set from URL parameter:', language);
             } else {
                 let local_lang = localStorage.getItem(`${appName}_language`);
                 if (local_lang) {
                     language = local_lang;
-                    console.log('Language set from localStorage:', language);
+                    console.log('startGCSDKs.js - Language set from localStorage:', language);
                 } else {
-                    console.log('Language not found in both URL parameters and localStorage.');
+                    console.log('startGCSDKs.js - Language not found in both URL parameters and localStorage.');
                 }
             }
             if (searchParams.has(qParamGcHostOrigin)) {
                 gcHostOrigin = searchParams.get(qParamGcHostOrigin);
                 localStorage.setItem(`${appName}_gcHostOrigin`, gcHostOrigin);
-                console.log('gcHostOrigin set from URL parameter:', gcHostOrigin);
+                console.log('startGCSDKs.js - gcHostOrigin set from URL parameter:', gcHostOrigin);
+                return gcHostOrigin;
             } else {
                 let local_gcHostOrigin = localStorage.getItem(`${appName}_gcHostOrigin`);
                 if (local_gcHostOrigin) {
                     gcHostOrigin = local_gcHostOrigin;
-                    console.log('gcHostOrigin set from localStorage:', gcHostOrigin);
+                    console.log('startGCSDKs.js - gcHostOrigin set from localStorage:', gcHostOrigin);
+                    return gcHostOrigin;
                 } else {
-                    console.log('gcHostOrigin not found in both URL parameters and localStorage.');
+                    console.log('startGCSDKs.js - gcHostOrigin not found in both URL parameters and localStorage.');
+                    return null;
                 }
             }
-            for (let pair of searchParams.entries()) {
-                console.log(pair[0] + ': ' + pair[1]); 
-            }
-            console.log('Returning gcHostOrigin:', gcHostOrigin);
-            return gcHostOrigin;
         }
     });
 }
