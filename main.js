9ccb0d29566ed31c6aa7bf22e17cd3195018db72
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-05-07 18:38:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -107,10 +107,14 @@ function showToast(message, toastId, duration = 5000) {
     // Check if the toast is being shown in a Salesforce environment
     const isSalesforce = getURLParameter('sf') === 'true';
 
+    console.log("main.js - showToast - Is Salesforce environment:", isSalesforce);
+
     if (isSalesforce) {
+        console.log("main.js - showToast - Creating custom toast for Salesforce LWC or other environments different than the native GC UI.");
         // Create a custom toast for Salesforce LWC or other environments different than the native GC UI
         createCustomToast(message, toastId, duration);
     } else {
+        console.log("main.js - showToast - Using clientApp SDK toast logic when not embedded in Salesforce.");
         // Use clientApp SDK toast logic when not embedded in Salesforce
         var options = {
             id: toastId,
