cc23153738dad4ac7dd64aa03258cf719a622b60
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-01 09:16:15 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,20 @@
+import '@/app/page.module.css'
+
+export const metadata = {
+  title: 'Monitor alerts',
+  description: 'Genesys Cloud monitor alerts application',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Monitor alerts</title>
+        <script src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"></script>
+        <script src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.3/purecloud-client-app-sdk-de77761d.min.js"></script>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
