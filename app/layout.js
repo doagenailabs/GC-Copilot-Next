407c05dceb3d69ddd0e0e0790e696958a4d1f2a0
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-01 09:10:40 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Monitor alerts</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
