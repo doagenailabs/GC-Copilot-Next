734e6bf4826b786e1334bcf6ac107085b63d9d2d
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-02 12:23:54 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import SDKLoader from '../components/SDKLoader'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'GC Copilot Next',
@@ -10,9 +10,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script
+          src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.3/purecloud-client-app-sdk.min.js"
+          strategy="beforeInteractive"
+        />
       </head>
       <body>
-        <SDKLoader />
         {children}
       </body>
     </html>
