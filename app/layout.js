eedaa9c8b3336594a3f3817c91865adf25ea702d
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-02 10:02:59 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'GC Copilot Next', 
+  title: 'GC Copilot Next',
   description: 'GC Copilot Next',
 }
 
@@ -9,8 +9,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"></script>
-        <script src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.7/purecloud-client-app-sdk.js"></script>
+        <script 
+          src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"
+          defer
+        ></script>
+        <script 
+          src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.7/purecloud-client-app-sdk.js"
+          defer
+        ></script>
       </head>
       <body>
         {children}
