b99864de92bb0980e772a0c0cb18e7cebe11145a
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-02 10:56:03 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,14 +10,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script 
+          src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.3/purecloud-client-app-sdk.js"
+          async={false}
+          defer={false}
+          crossOrigin="anonymous"
+        />
+        <script 
           src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"
           async={false}
           defer={false}
-        />
-        <script 
-          src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.3/purecloud-client-app-sdk-de77761d.min.js"
-          async={false}
-          defer={false}
+          crossOrigin="anonymous"
         />
       </head>
       <body>{children}</body>
