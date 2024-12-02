58e3d418646b947be8efbcd44b5095ce6415a396
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-02 19:30:49 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,10 @@
-import Script from 'next/script';
+import ScriptsLoader from '../components/ScriptsLoader';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.7/purecloud-client-app-sdk.min.js"
-          strategy="beforeInteractive"
-        />
+        {/* Other head elements */}
       </head>
       <body>{children}</body>
     </html>
