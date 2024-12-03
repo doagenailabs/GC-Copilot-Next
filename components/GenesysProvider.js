43481f827f3a2b00f440c28468b6671574ec7b7e
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-03 14:24:08 +0100

    Create GenesysProvider.js

---
+++ components/GenesysProvider.js
@@ -0,0 +1,16 @@
+import React, { createContext, useContext } from 'react';
+import { useGenesysSDK } from '../hooks/useGenesysSDK';
+
+const GenesysContext = createContext({});
+
+export const useGenesys = () => useContext(GenesysContext);
+
+export const GenesysProvider = ({ children }) => {
+  const sdkState = useGenesysSDK();
+
+  return (
+    <GenesysContext.Provider value={sdkState}>
+      {children}
+    </GenesysContext.Provider>
+  );
+};
