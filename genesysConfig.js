753aea9035fc767cec259f886a4552b7d32ac427
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-03 17:12:47 +0100

    Update and rename lib/genesysConfig.js to genesysConfig.js

---
+++ genesysConfig.js
@@ -0,0 +1,7 @@
+window.GENESYS_CONFIG = {
+  clientId: '',
+  redirectUri: window.location.origin,
+  appName: 'GCCopilotNext',
+  defaultEnvironment: 'mypurecloud.ie',
+  defaultLanguage: 'en-us'
+};
