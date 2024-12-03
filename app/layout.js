f78ce30c7cce75e8bf2aeb85992fae4e117f463f
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-03 14:33:54 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
 import Script from 'next/script';
 import { GenesysProvider } from '../components/GenesysProvider';
 
+export const metadata = {
+  title: 'GCCopilotNext',
+  description: 'Genesys Cloud Copilot Next.js Application',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
       <body>
         <Script
           src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"
