0b4432fb832efca130b2e870c0142ec548004b9e
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-03 17:37:59 +0100

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,6 +1,7 @@
 module.exports = (req, res) => {
     res.json({
         GCclientId: process.env.GC_OAUTH_CLIENT_ID,
-        logsEnabled: process.env.LOGS_ENABLED
+        logsEnabled: process.env.LOGS_ENABLED,
+        maxHistoryMessages: process.env.CONVERSATION_HISTORY_MESSAGES_NUMBER
     });
 };
