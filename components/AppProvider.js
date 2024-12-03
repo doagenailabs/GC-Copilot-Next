8109324e1c3e00e456b88b18d61295b40dac4b5c
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-03 15:18:21 +0100

    Update AppProvider.js

--- components/AppProvider.js
+++ components/AppProvider.js
@@ -1,21 +1,11 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import { GenesysProvider } from './GenesysProvider';
-
-// Dynamically import GenesysScripts with no SSR
-const GenesysScripts = dynamic(
-  () => import('./GenesysScripts'),
-  { ssr: false }
-);
 
 export function AppProvider({ children }) {
   return (
-    <>
-      <GenesysScripts />
-      <GenesysProvider>
-        {children}
-      </GenesysProvider>
-    </>
+    <GenesysProvider>
+      {children}
+    </GenesysProvider>
   );
 }
