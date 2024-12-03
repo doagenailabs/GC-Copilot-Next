1e71f6674b48e4d6da9db7d22b03f188424440d8
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-12-03 14:06:59 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,22 @@
+import Script from 'next/script';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"></script>
-        <script src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.7/purecloud-client-app-sdk.js"></script>
-      </head>
-      <body>{children}</body>
+      <head />
+      <body>
+        <Script
+          src="https://sdk-cdn.mypurecloud.com/javascript/latest/purecloud-platform-client-v2.min.js"
+          strategy="beforeInteractive"
+          id="gc-platform-sdk"
+        />
+        <Script
+          src="https://sdk-cdn.mypurecloud.com/client-apps/2.6.7/purecloud-client-app-sdk.js"
+          strategy="beforeInteractive"
+          id="gc-client-app-sdk"
+        />
+        {children}
+      </body>
     </html>
   );
 }
