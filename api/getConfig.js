d532e56997b9f9377ba2b2e24bcee07f30156076
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2024-12-04 10:45:43 +0100

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,7 +1,11 @@
 module.exports = (req, res) => {
-    res.json({
+    const config = {
         GCclientId: process.env.GC_OAUTH_CLIENT_ID,
         logsEnabled: process.env.LOGS_ENABLED,
         maxHistoryMessages: process.env.CONVERSATION_HISTORY_MESSAGES_NUMBER
-    });
+    };
+    
+    console.log('API: Returning config:', config);
+    
+    res.json(config);
 };
